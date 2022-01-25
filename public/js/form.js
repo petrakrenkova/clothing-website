021b2ca819bb61a393ade2e1d0798aeefd2172a5
Matěj Křenek <mate23.krenek@gmail.com>
2022-01-25 01:24:16 +0100

    commit

---
+++ public/js/form.js
@@ -0,0 +1,31 @@
+// select inputs
+const submitBtn = document.querySelector('.submit-btn');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const number = document.querySelector('#number');
+const tac = document.querySelector('#terms-and-cond');
+const notification = document.querySelector('#notification');
+
+submitBtn.addEventListener('click', () => {
+    if(name.value.length < 3){
+        showAlert('name must be 3 letters long');
+    } else if(!email.value.length){
+        showAlert('enter your email');
+    } else if(password.value.length < 8){
+        showAlert('password should be 8 letters long');
+    } else if(!number.value.length){
+        showAlert('enter your phone number');
+    }
+})
+
+// alert function
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+}
